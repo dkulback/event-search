8102320a2623601c5f9b98fd74e92cb575c496b2
Darren Kulback <dkulback@gmail.com>
2022-01-19 23:23:05 -0700

    remove end

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'users/new.html.erb', type: :view do
   it 'has a form to register a user' do
     visit root_path
-    save_and_open_page
+
     within '.new-user' do
     end
   end
